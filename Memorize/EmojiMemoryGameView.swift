7038ef029360d3fabd99f04ca80637f2ff006835
komiselena <komiselen@yandex.ru>
2023-11-28 20:35:46 +0300

    pie and clarify

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
+    typealias Card = MemoryGame<String>.Card
+    
+    private let aspectRatio: CGFloat = 3/4
+    private let spacing: CGFloat = 4
     
     var body: some View {
         
@@ -36,44 +40,18 @@ struct EmojiMemoryGameView: View {
     }
 
     
-    var cards: some View{
-        LazyVGrid(columns:[GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card: card)
-                    .aspectRatio(3/4, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
+    private var cards: some View{
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio){ card in
+            CardView(card: card)
+                .padding(spacing)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
             }
-        }
-        .foregroundColor(Color.orange)
+        .foregroundColor(viewModel.color)
     }
 }
-    
-struct CardView: View {
-    let card: MemoryGame<String>.Card
 
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode: .fit)
-            }
-                .opacity(card.isFaceUp ? 1 : 0)
-            base.fill()
-                .opacity(card.isFaceUp ? 0 : 1)
-                
-            }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
-        
-        }
-    }
 // Структура того как выглядят карточки
     
 
